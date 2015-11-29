d151a050452f829e1292525b7876b19eaebb271e
Leonardo Rojas <leonardo.rojas@fairplaylabs.com>
2015-11-29 12:27:11 -0600

    Fixing bugs.

--- Mockup/scripts/web-service/WebServiceClient.js
+++ Mockup/scripts/web-service/WebServiceClient.js
@@ -34,16 +34,32 @@ WebServiceClientClass.prototype.deleteContact = function(id, callback)
 
 WebServiceClientClass.prototype.ajax = function(method, resourceURI, callback)
 {
+    var thisVar = this;
+
 	$.ajax(
 	{ 
         type: method, 
         url: this.serverURL + resourceURI, 
         dataType: "json",
-        success: function(data, status, xhr) { callback(data); },
+        data: null,
+        success: function(data, status, xhr) 
+        { 
+            if(!data.Success)
+                thisVar.logError(data);
+
+            callback(data); 
+        },
         error:   function(data, status, xhr) 
         {
-        	var errorData = { Success : false, Data : data }; 
+        	var errorData = { Success : false, Data : data };
+            
+            thisVar.logError(errorData);
         	callback(errorData); 
         }
     });
 };
+
+WebServiceClientClass.prototype.logError = function(errorData)
+{
+    console.log(JSON.stringify(errorData));
+}
