32d77b84f009180cc41c54d6907d04bc210825d2
Leonardo Rojas <leonardo.rojas@fairplaylabs.com>
2015-11-29 00:51:07 -0600

    Implementing add contact dialog.

--- Mockup/scripts/AddContactDialog.js
+++ Mockup/scripts/AddContactDialog.js
@@ -1,14 +1,30 @@
 //Variables
 AddContactDialog.prototype.addContactDiv = null;
+AddContactDialog.prototype.onAddClicked  = null;
 
 //Constructors
 function AddContactDialog()
 {
 	this.addContactDiv = $("#addContactDialog");
+
+	var thisVar = this;
+	$("#addContactDialogButton").click(function() { thisVar.onAddContactButtonClick(); });
+}
+
+AddContactDialog.prototype.onAddContactButtonClick = function()
+{
+ 	this.onAddClicked($("#name").val(), $("#email").val(), $("#password").val());
+ 	this.hide();
 }
 
 //Methods
-AddContactDialog.prototype.show = function()
+AddContactDialog.prototype.show = function(onAddClicked)
 {
-	$("#addContactDialog").dialog();
+	this.onAddClicked = onAddClicked;
+	this.addContactDiv.dialog();
 };
+
+AddContactDialog.prototype.hide = function()
+{
+	this.addContactDiv.hide();
+}
